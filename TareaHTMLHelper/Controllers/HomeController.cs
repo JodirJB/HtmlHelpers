b5c7b7fe6b1697d2bfe24af05ea309b1be29122d
JodirJB <48960585+JodirJB@users.noreply.github.com>
2019-03-30 23:07:14 -0400

    versiion 1

---
+++ TareaHTMLHelper/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TareaHTMLHelper.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(Models.Formulario fmo)
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Helper()
+        {
+           return View();
+        }
+    }
+}
---
+++ TareaHTMLHelper/Models/Formulario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TareaHTMLHelper.Models
+{
+    public class Formulario
+    {
+        [Required]
+        public String Cedula { get; set; }
+        [MinLength(11)]
+
+        [Required]
+        public String Nombre { get; set; }
+
+        [Required]
+        public String Apellido { get; set; }
+
+        [Range(15, 100)]
+        public int Edad { get; set; }
+
+        public String Telefono { get; set; }
+        [MinLength(10)]
+
+        public String CorreoElectronico { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Por favor coloque su Email")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Este Email no es validad.")]
+
+        public String Genero { get; set; }
+
+        public String EstadoCivil { get; set; }
+
+        public String Hobbys { get; set; }
+    }
+}
